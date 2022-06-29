ea9d15d4bfd12d528f2340bfbe60e4c01342060a
ondjoss <ghislain.chakam@gmail.com>
2022-06-29 10:12:40 +0200

    Ajout des différentes routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import UserList from './pages/UserList';
 
 
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello La Team Reacct !</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='favoris' element={<UserList/>}/>
+        <Route path='*' element={<Home/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 };
+
 
 export default App;
