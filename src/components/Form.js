a1b49b42af28ada00968bb7bd14e44a67cf4bf3e
ondjoss <ghislain.chakam@gmail.com>
2022-07-07 00:16:45 +0200

    Ajout de la fonctionnalité search avec le CallBack...

--- src/components/Form.js
+++ src/components/Form.js
@@ -7,18 +7,19 @@ import Card from './Card';
 const Form = () => {
 
     const [filmData, setFilmData] = useState([]);
+    const [search, setSearch] = useState("code");
 
     useEffect(() => {
-        axios.get(`https://api.themoviedb.org/3/search/movie?api_key=1b53a67abc248fbeb4ccccf2044cb1a3&query=start&language=fr-FR`
+        axios.get(`https://api.themoviedb.org/3/search/movie?api_key=1b53a67abc248fbeb4ccccf2044cb1a3&query=${search}&language=fr-FR`
         ).then((res) => setFilmData(res.data.results));
-    }, []);
+    }, [search]);
 
     return (
         <div className="form-component">
             <div className="form-container">
                 <form action="">
                     <input type="text" placeholder='Entrez le titre du film' 
-                    id='search-input'/>
+                    id='search-input' onChange={(e) => setSearch(e.target.value)} />
                     <input type="submit" value="Rechercher"/>
                 </form>
                 <div className="btn-sort-container">
